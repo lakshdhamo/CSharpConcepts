804b192c39706ded00e868a91ddad2169f232601
Lakshmanan D <56861446+lakshdhamo@users.noreply.github.com>
2021-12-03 20:28:56 +0530

    Add project files.

---
+++ CSharpConcepts/Threading/Semaphore.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace CSharpConcepts.Threading
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Semaphore semaphoreObject = new Semaphore(initialCount: 3, maximumCount: 3, name: "PrinterApp");
+            Printer printerObject = new Printer();
+
+            for (int i = 0; i < 20; ++i)
+            {
+                int j = i;
+                Task.Factory.StartNew(() =>
+                {
+                    semaphoreObject.WaitOne();
+                    printerObject.Print(j);
+                    semaphoreObject.Release();
+                });
+            }
+            Console.ReadLine();
+        }
+    }
+
+    class Printer
+    {
+        public void Print(int documentToPrint)
+        {
+            Console.WriteLine("Printing document: " + documentToPrint);
+            //code to print document
+            Thread.Sleep(TimeSpan.FromSeconds(5));
+        }
+    }
+}
